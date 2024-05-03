0c7fb3fcc8c22d9fd72f29ef72204bbcb96ebcba
Sergey <mysteryon88@yandex.ru>
2024-05-03 15:58:25 +0300

    flatten contracts

---
+++ flatten/Treasury_flattened.sol
@@ -0,0 +1,361 @@
+
+// File: @openzeppelin/contracts/utils/introspection/IERC165.sol
+
+
+// OpenZeppelin Contracts (last updated v5.0.0) (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+// File: @openzeppelin/contracts/token/ERC721/IERC721.sol
+
+
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC721/IERC721.sol)
+
+pragma solidity ^0.8.20;
+
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC721 is IERC165 {
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon
+     *   a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must have been allowed to move this token by either {approve} or
+     *   {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon
+     *   a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+
+    /**
+     * @dev Transfers `tokenId` token from `from` to `to`.
+     *
+     * WARNING: Note that the caller is responsible to confirm that the recipient is capable of receiving ERC721
+     * or else they may be permanently lost. Usage of {safeTransferFrom} prevents loss, though the caller must
+     * understand this adds an external call which potentially creates a reentrancy vulnerability.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 tokenId) external;
+
+    /**
+     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
+     * The approval is cleared when the token is transferred.
+     *
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
+     *
+     * Requirements:
+     *
+     * - The caller must own the token or be an approved operator.
+     * - `tokenId` must exist.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address to, uint256 tokenId) external;
+
+    /**
+     * @dev Approve or remove `operator` as an operator for the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
+     *
+     * Requirements:
+     *
+     * - The `operator` cannot be the address zero.
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setApprovalForAll(address operator, bool approved) external;
+
+    /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    /**
+     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
+     *
+     * See {setApprovalForAll}
+     */
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
+
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
+
+
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+// File: contracts/DAO/miniDAO/utils/ERC721TokenReceiver.sol
+
+
+pragma solidity ^0.8.23;
+
+/// @notice A generic interface for a contract which properly accepts ERC721 tokens.
+abstract contract ERC721TokenReceiver {
+    /// @notice Handles the receipt of an NFT.
+    /// @return Returns the selector to confirm the interface and proper receipt.
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return ERC721TokenReceiver.onERC721Received.selector;
+    }
+}
+
+// File: contracts/DAO/miniDAO/utils/Owned.sol
+
+
+pragma solidity >=0.8.0;
+
+abstract contract Owned {
+    address public owner;
+
+    modifier onlyOwner() virtual {
+        require(msg.sender == owner, "UNAUTHORIZED");
+        _;
+    }
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+}
+
+// File: contracts/DAO/miniDAO/Treasury.sol
+
+
+pragma solidity ^0.8.23;
+
+/// @title Treasury Contract
+/// @notice This contract serves as a treasury to manage and distribute native tokens and ERC20 tokens.
+/// @dev This contract allows the owner to release native tokens and ERC20 tokens to specified recipients.
+/// It inherits functionality from Owned, ERC721TokenReceiver, and ERC1155TokenReceiver contracts.
+contract Treasury is Owned, ERC721TokenReceiver {
+    /// @notice Constructs the Treasury contract with a specified timelock contract address.
+    /// @param _timelock The address of the timelock contract.
+    constructor(address _timelock) payable Owned(_timelock) {}
+
+    /// @notice Sends a specified amount of the native blockchain currency to a given address
+    /// @dev Ensures there are sufficient funds in the contract before attempting to send.
+    /// @param to The recipient's address to which the native currency will be sent
+    /// @param amount The amount of native currency to be sent
+    function releaseNativeToken(address to, uint256 amount) public onlyOwner {
+        // Verify the contract has enough native currency to complete the transfer
+        require(address(this).balance >= amount, "insufficient funds");
+
+        // Transfer the specified amount of native currency to the recipient
+        payable(to).transfer(amount);
+    }
+
+    /// @notice Transfers a specified amount of ERC20 tokens to a given address
+    /// @dev This function requires that the contract holds enough tokens before attempting transfer.
+    /// @param to The recipient's address to which the tokens will be transferred
+    /// @param amount The amount of ERC20 tokens to be transferred
+    /// @param token The address of the ERC20 token contract
+    function releaseERC20Token(
+        address to,
+        uint256 amount,
+        address token
+    ) public onlyOwner {
+        IERC20 ERC20Token = IERC20(token);
+
+        // Checks the contract's token balance to ensure it has enough tokens to transfer
+        require(
+            ERC20Token.balanceOf(address(this)) >= amount,
+            "insufficient funds"
+        );
+
+        // Attempts to transfer the tokens and requires that the transfer is successful
+        require(ERC20Token.transfer(to, amount), "Transfer failed");
+    }
+
+    /// @notice Transfers an ERC721 token from the contract to a specified address
+    /// @dev Requires that the contract is the owner of the token and that it possesses at least one token.
+    /// @param to The recipient address of the ERC721 token
+    /// @param id The token ID of the ERC721 token to transfer
+    /// @param token The address of the ERC721 token contract
+    function releaseERC721oken(
+        address to,
+        uint256 id,
+        address token
+    ) public onlyOwner {
+        IERC721 ERC721Token = IERC721(token);
+
+        // Ensure the contract has at least one token to transfer
+        require(ERC721Token.balanceOf(address(this)) > 0, "insufficient funds");
+
+        // Ensure the contract is the current owner of the token id
+        require(
+            ERC721Token.ownerOf(id) == address(this),
+            "We don't own the id"
+        );
+
+        // Execute the safe transfer of the token to the specified address
+        ERC721Token.safeTransferFrom(address(this), to, id, "0x00");
+    }
+
+    /// @dev Fallback function to receive native tokens.
+    receive() external payable {}
+}
