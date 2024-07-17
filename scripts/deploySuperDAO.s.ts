f247c572cc6ba6951a79350b17af89cf88648694
Sergey <mysteryon88@yandex.ru>
2024-07-17 19:13:48 +0300

    deploy

--- scripts/deploySuperDAO.s.ts
+++ scripts/deploySuperDAO.s.ts
@@ -8,12 +8,16 @@ async function main() {
     "TokenDAO", // название токена для ДАО
     "TKN", // символ токена для ДАО
     "MiniDAO name", // название ДАО
-    50, // _votingDelay - задержка в секундах между созданием предложения и началом голосования
-    100, // _votingPeriod - длительность голосования в секундах
+    5, // _votingDelay - задержка в секундах между созданием предложения и началом голосования
+    15, // _votingPeriod - длительность голосования в секундах
     4 // _quorumValue - какой процент должен быть собран для успешного завершения голосования
   );
 
+  const ERC721 = await ethers.getContractFactory("MockTokenERC721");
+  const erc721 = await ERC721.deploy();
+
   console.log(`SuperDAO deployed to ${await superDAO.getAddress()}`);
+  console.log(`ERC721 deployed to ${await erc721.getAddress()}`);
 }
 
 main().catch((error) => {
